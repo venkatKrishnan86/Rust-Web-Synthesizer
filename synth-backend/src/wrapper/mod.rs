f5bb2364da094cddd198a6be272dd207021c6e74
venkatKrishnan86 <88517272+venkatKrishnan86@users.noreply.github.com>
2024-04-24 16:13:08 -0400

    Added MultiOscillator functions to be used in synth

--- synth-backend/src/wrapper/mod.rs
+++ synth-backend/src/wrapper/mod.rs
@@ -1,16 +1,19 @@
-use crate::oscillators::MultiOscillator;
+use crate::oscillators::{MultiOscillator, Oscillator, WaveTableOscillator};
 use crate::filters::{Filter, FilterType};
+use std::ops::Add;
 
 #[derive(Clone, Debug)]
 pub struct Synth {
     pub osc: MultiOscillator,
+    pub sample_rate: u32,
     pub filter: Option<Filter>, // Make filter an optional field
 }
 
 impl Synth {
-    pub fn new(osc: MultiOscillator, filter: Option<Filter>) -> Self {
+    pub fn new(osc: MultiOscillator, sample_rate: u32, filter: Option<Filter>) -> Self {
         Self {
             osc,
+            sample_rate,
             filter,
         }
     }
@@ -28,5 +31,21 @@ impl Synth {
 
         // If filter is None, return the sample directly
         sample
+    }
+
+    pub fn set_oscillator(&mut self, index: usize, oscillator: Oscillator) {
+        self.osc.set_oscillator(index, oscillator);
+    }
+
+    pub fn push(&mut self, oscillator: WaveTableOscillator) -> Result<(), String> {
+        self.osc.push(oscillator)
+    }
+
+    pub fn global_set_frequency(&mut self, frequency: f32) -> Result<(), String> {
+        self.osc.global_set_frequency(frequency)
+    }
+
+    pub fn num_sources(&self) -> usize {
+        self.osc.num_sources()
     }
 }
